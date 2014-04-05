d231b7f82b9ce38d65660aad09568db9edffddcb
Benjamin Edwards <edwards.benw@gmail.com>
2014-04-05 10:55:43 -0400

    obtain reference to edit text widget in the activity

--- app/src/main/java/com/example/mydemoapp/app/MainActivity.java
+++ app/src/main/java/com/example/mydemoapp/app/MainActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+
+    private EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (EditText) findViewById(R.id.editText1);
     }
 
 
