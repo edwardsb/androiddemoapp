c3a3f77e368011f264823c983b937792f883a327
Benjamin Edwards <edwards.benw@gmail.com>
2014-04-05 11:47:38 -0400

    added text view reference to the result activty, set text to be the result from the previous activity

--- app/src/main/java/com/example/mydemoapp/app/ResultActivity.java
+++ app/src/main/java/com/example/mydemoapp/app/ResultActivity.java
@@ -1,15 +1,25 @@
 package com.example.mydemoapp.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ResultActivity extends Activity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        textView = (TextView) findViewById(R.id.result_text);
+        Intent intent = getIntent();
+        float result = intent.getFloatExtra(MainActivity.RESULT, 0);
+        Log.d("RESULT PASSED", String.valueOf(result));
+        textView.setText(String.valueOf(result));
+
     }
 }
